a56f3e5d93fc0a572d229724685a3553e663ed1e
MemoInsanito123 <martinez.vaalles.guillermo11@gmail.com>
2024-11-03 22:25:02 -0600

    version0.1

--- public/scripts/insertBreads.js
+++ public/scripts/insertBreads.js
@@ -1,7 +1,8 @@
 
 //Guardar el formulario y asignarle un evento submit sin recargar
 const formulario = document.getElementById('form_pan').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita que el formulario se envíe de forma tradicional
+    
+    //event.preventDefault(); // Evita que el formulario se envíe de forma tradicional
 
     const pan = {
         nombre_pan : document.getElementById('nombre_pan').value,
@@ -33,12 +34,11 @@ const formulario = document.getElementById('form_pan').addEventListener('submit'
 
 );
 
-
+//Crear la tabla cuando inicia el Admin.html y cargas los datos de la DB
 const showPanes = () => {
-
     let panes = {};
 
-    fetch('products/GET')
+    fetch('products/GET/Admin')
     .then(response => response.json())
     .then(data => {
         panes = data;
@@ -53,17 +53,134 @@ const showPanes = () => {
         array1.forEach(element => {
             
             //Crear elementos de la tabla
-            
             const fila = document.createElement('tr');
             
             const columnaNombre = document.createElement('th');
             const columnaDescripcion = document.createElement('th');
+            const columnaPrecio = document.createElement('th');
+            const columnaCantidad = document.createElement('th');
+            const columnaImagen = document.createElement('th');
+            const columnaAccion = document.createElement('th');
 
+            const imagen = document.createElement('img');
+            const boton00 = document.createElement('button');
+            const boton01 = document.createElement('button');
+
+            //Agregamos una funcion para cuando clickees el boton editar
+            boton00.onclick = () => {
+                //Guardar el Id del elemento 
+                const id_pan_now = element.id_pan;
+                //Obtener el modal del admin.html
+                const modal = document.querySelector('#modal');
+                
+                //Agregar un evento de cancelar
+                const boton_cancelar = document.getElementById('cancelar_dialog');
+                boton_cancelar.addEventListener('click', () => {
+                    modal.close();
+                });
+
+                //Agregamos una funcion para cuando se envie el formulario del modal
+                const formulario = document.getElementById('form_pan_update').addEventListener('submit',() =>{
+
+                    const pan_update = {
+                        id_pan : id_pan_now,
+                        nombre_pan : document.getElementById('nombre_pan_update').value,
+                        descripcion_pan : document.getElementById('descripcion_pan_update').value,
+                        precio_pan : parseInt(document.getElementById('precio_pan_update').value),
+                        cantidad_stock : parseInt(document.getElementById('cantidad_pan_update').value),
+                        img_pan : document.getElementById('imagen_pan_update').value
+                    }
+
+                    fetch(
+                        'products/PUT',
+                        {
+                            method : 'PUT',
+                            headers : {'Content-Type': 'application/json'},
+                            body : JSON.stringify(pan_update)
+                        }
+                    )
+                    .then(response => {
+                        if (response.ok) {
+                            return response.json();
+                        }
+                        throw new Error('Error en la petición');
+                    })
+                    .then(data => {
+                        console.log('Pan Actualizado ');
+                    })
+                    .catch(error => console.error('Error:', error));
+
+                });
+
+                //Mostrar el Modal
+                modal.show();
+
+                //Mostrar el ID del pan
+                console.log(id_pan_now);
+            };
+
+            boton01.addEventListener('click', () => {
+
+                const id_pan_now = element.id_pan;
+
+                const pan_delete = {id_pan : id_pan_now};
+
+                const modal = document.getElementById('modal1');
+                const boton_cancelar = document.getElementById('cancelar_dialog1');
+
+                boton_cancelar.addEventListener('click', () => {
+                    modal.close();
+                });
+
+                fetch(
+                    'products/DELETE',
+                    {
+                        method : 'DELETE',
+                        headers : {'Content-Type': 'application/json'},
+                        body : JSON.stringify(pan_delete)
+                    }
+                )
+                .then(response => {
+                    if (response.ok) {
+                        return response.json();
+                    }
+                    throw new Error('Error en la petición');
+                })
+                .then(data => {
+                    console.log('Pan Actualizado ');
+                })
+                .catch(error => console.error('Error:', error));
+
+                modal.show();
+
+            });
+
+            //Agregar atributos a los componentes creados
+            columnaNombre.textContent = element.nombre_pan;
+            columnaDescripcion.textContent = element.descripcion_pan;
+            columnaPrecio.textContent = element.precio_pan;
+            columnaCantidad.textContent = element.cantidad_stock;
+            imagen.src = element.img_pan;
+
+            boton00.textContent = 'Editar';
+            boton01.textContent = 'Eliminar';
+
+            //Anidar elementos como hijos de otros
+            columnaImagen.appendChild(imagen);
+            columnaAccion.appendChild(boton00);
+            columnaAccion.appendChild(boton01);
+
+            fila.appendChild(columnaNombre);
+            fila.appendChild(columnaDescripcion);
+            fila.appendChild(columnaPrecio);
+            fila.appendChild(columnaCantidad);
+            fila.appendChild(columnaImagen);
+            fila.appendChild(columnaAccion);
             
-
+            tbody.append(fila);
         });
-
     }
-
 };
+
+showPanes();
 
--- routes/methodProducts.js
+++ routes/methodProducts.js
@@ -16,6 +16,20 @@ router.get('/GET', (req, res) => {
 });
 
 
+// Ruta para obtener todos los productos para el Administrador (incluye el ID del pan)
+router.get('/GET/Admin', (req, res) => {
+    const query = 'SELECT pan.id_pan, pan.nombre_pan, pan.descripcion_pan, precio_pan.precio_pan, stock_pan.cantidad_stock, pan.img_pan FROM pan JOIN precio_pan ON precio_pan.id_pan = pan.id_pan JOIN stock_pan ON stock_pan.id_pan = pan.id_pan';
+
+    db.execute(query, (err, results) => {
+        if (err) {
+            return res.status(500).send('Error en la consulta');
+        }
+
+        res.json(results); // Devuelve los resultados como JSON
+    });
+});
+
+//Metodo POST, insertar pan, cantidad y stock en la base de datos
 router.post('/POST', (req, res) => {
     db.beginTransaction((err) => {
         
@@ -59,17 +73,85 @@ router.post('/POST', (req, res) => {
     })
 });
 
+//Actualizar un registro del pan en BD
+router.put('/PUT', (req, res) => {
+    db.beginTransaction((err) => {
+        if (err) return res.status(500).send('Error al iniciar transacción');
 
-router.get('/GET/Admin', (req, res) => {
-    const query = 'SELECT pan.id_pan pan.nombre_pan, pan.descripcion_pan, precio_pan.precio_pan, stock_pan.cantidad_stock, pan.img_pan FROM pan JOIN precio_pan ON precio_pan.id_pan = pan.id_pan JOIN stock_pan ON stock_pan.id_pan = pan.id_pan';
+        const {id_pan, nombre_pan, descripcion_pan, img_pan} = req.body;
+        const query0 = 'UPDATE pan SET nombre_pan = ?, descripcion_pan = ?, img_pan = ? WHERE id_pan = ?';
 
-    db.execute(query, (err, results) => {
-        if (err) {
-            return res.status(500).send('Error en la consulta');
-        }
+        db.execute(query0, [nombre_pan, descripcion_pan, img_pan, id_pan], (err, response) => {
+            if(err){
+                return db.rollback(() => res.status(500).send('Error al actualizar pan'));
+            }
 
-        res.json(results); // Devuelve los resultados como JSON
+            const {precio_pan} = req.body;
+            const query1 = 'UPDATE precio_pan SET precio_pan = ? WHERE id_pan = ?';
+
+            db.execute(query1, [precio_pan, id_pan], (err) => {
+                if(err){
+                    return db.rollback(() => res.status(500).send('Error al actualizar precio pan'));
+                }
+
+                const {cantidad_stock} = req.body;
+                const query2 = 'UPDATE stock_pan SET cantidad_stock = ? WHERE id_pan = ?';
+
+                db.execute(query2, [cantidad_stock, id_pan], (err) => {
+                    
+                    if(err){
+                        return db.rollback(() => res.status(500).send('Error al actualizar cantidad pan'));
+                    }
+
+                    db.commit((err) => {
+                        if (err) return db.rollback(() => res.status(500).send('Error al confirmar transacción'));
+                        res.status(201).send({ mensaje: 'Pan, precio y cantidad actualizados exitosamente'});
+                    })
+                });
+            });
+        });
     });
 });
+
+//Eliminar un pan de la DB
+router.delete("/DELETE", (req, res) => {
+    const {id_pan} = req.body;
+
+    db.beginTransaction(err => {
+        if (err) return res.status(500).send('Error al iniciar transacción');
+
+        const query0 = 'DELETE FROM precio_pan WHERE id_pan = ?';
+
+        db.execute(query0, [id_pan], (err, response) => {
+            if(err){
+                return db.rollback(() => res.status(500).send('Error al eliminar precio pan'));
+            }
+
+            const query1 = 'DELETE FROM stock_pan WHERE id_pan = ?';
+
+            db.execute(query1, [id_pan], (err) => {
+                if(err){
+                    return db.rollback(() => res.status(500).send('Error al eliminar stock pan'));
+                }
+
+                const query2 = 'DELETE FROM pan WHERE id_pan = ?';
+
+                db.execute(query2, [id_pan], (err) => {
+                    if(err){
+                        return db.rollback(() => res.status(500).send('Error al eliminar pan'));
+                    }
+
+                    db.commit((err) => {
+                        if (err) return db.rollback(() => res.status(500).send('Error al confirmar transacción'));
+                        res.status(201).send({ mensaje: 'Pan, precio y cantidad eliminados exitosamente'});
+                    })
+                });
+            });
+        });
+    });
+
+});
+
+
 
 module.exports = router;
